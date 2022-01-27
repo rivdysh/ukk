2fb9b9c0256668d2007844580411a03b31b561fa
rivdysh <93703272+rivdysh@users.noreply.github.com>
2022-01-27 11:07:30 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$konek = mysqli_connect("127.0.0.1", "root", "", "ukk2022");
+
+if ($konek) {
+    print "terhubung";
+} else {
+    print "tidak terhubung...";
+}
---
+++ konten/registrasi.php
@@ -0,0 +1,25 @@
+<div class="row">
+        <div class="col-md-4" offset-md-4>
+          <div class="card-header">REGISTRASI</div>
+          <div class="card-body">
+            <form action="" method="post">
+              <div class="from-group">
+                <input type="text" class="from-control" placeholder="NIK" required autofocus />
+              </div>
+              <div class="from-group">
+                <input type="text" class="from-control" placeholder="Nama Lengkap" required />
+              </div>
+              <div class="from-group">
+                <input type="text" class="from-control" placeholder="Username" required />
+              </div>
+              <div class="from-group">
+                <input type="password" class="from-control" placeholder="Password" required />
+              </div>
+              <div class="from-group">
+                <input type="number" class="from-control" placeholder="No.telepon" required />
+              </div>
+              <button type="submit" class="btn btn-primary">Kirim</button>
+            </form>
+          </div>
+        </div>
+      </div>
